fa1d04ca1d700d98ac65473859bc9179b2658d25
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-04-04 20:45:49 -0400

    trying to make it work!!

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ var $btn = $('.btn');
 var $btnClick = $('.btn-click');
 var $engage = $('.engage');
 var $engageClick = $('.engage-click');
+var $lines = $('.lines');
+var $linesOn = $('.lines-on');
 
 $btn.on('click', function () {
   $btn.toggleClass('btn-click');
+});
+
+$btn.on('click', function () {
   $engage.toggleClass('engage-click');
+});
+
+$btn.on('click', function () {
+  $lines.toggleClass('lines-on');
 });
