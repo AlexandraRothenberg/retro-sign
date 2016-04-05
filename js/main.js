827b29610ab4c92c20b70d4307436cc316c0d7fb
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-04-05 18:59:40 -0400

    trying to fix

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-var $body = $('body');
 var $svg = $('svg');
 var $btn = $('.btn');
-var $btnClick = $('.btn-click');
 var $engage = $('.engage');
-var $engageClick = $('.engage-click');
 var $lines = $('.lines');
-var $linesOn = $('.lines-on');
 
-$btn.on('click', function () {
+$svg.on('click', function () {
   $btn.toggleClass('btn-click');
-});
-
-$btn.on('click', function () {
   $engage.toggleClass('engage-click');
-});
-
-$btn.on('click', function () {
   $lines.toggleClass('lines-on');
 });
